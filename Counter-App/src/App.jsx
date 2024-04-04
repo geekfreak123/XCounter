806072d79a9df08f772b34258919fa8acb01c83d
geekfreak123 <sonawanerohan502@gmail.com>
2024-04-04 16:19:55 +0530

    Done

---
+++ Counter-App/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const Increment = () =>{
+    setCount(count+1)
+  }
+
+  const Decrement = () =>{
+    setCount(count-1)
+  }
+
+  return (
+    <div>
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button onClick={Increment}>Increment</button>
+        <button onClick={Decrement}>Decrement</button>
+    </div>
+  )
+}
+
+export default App
